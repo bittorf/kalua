080fb315d91305e715f038a54b339e0e0e1d5562
Bastian Bittorf <bittorf@bluebottle.com>
2011-08-05 21:30:11 +0200

    364603 sorry, first checkin of user_tunnel framework for internal tests 3/3

---
+++ www/cgi-bin-tunnel.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+. /tmp/loader
+
+if [ -z "$1" -a -n "$REMOTE_ADDR" ]; then
+	eval $( _http query_string_sanitize )		# ACTION=... MAC=... IP_USER=... HASH=... // ( TUNNEL_ID=... | IP_ROUTER=$REMOTE_ADDR )
+	_http header_mimetype_output "text/plain"
+else
+	ACTION="$1"
+fi
+
+_log do tunnel_helper daemon info "ACTION: $ACTION QUERY: $QUERY_STRING"
+
+case "$ACTION" in
+	tunnel_possible)
+		if _tunnel check_local_capable ; then
+			_watch counter "/tmp/tunnel_id" increment 1 max 65 || {		# fixme! set to 0 during nightly/kick_user_all()
+				echo "FALSE=;"
+				exit
+			}
+
+			read TUNNEL_ID <"/tmp/tunnel_id"
+
+			echo -n "TRUE=;"
+			echo -n "TUNNEL_IP_CLIENT=$( _tunnel id2ip $TUNNEL_ID client );"
+			echo -n "TUNNEL_IP_SERVER=$( _tunnel id2ip $TUNNEL_ID server );"
+			echo    "TUNNEL_MASK=30;"
+
+	#		eval $( _tunnel get_speed_for_hash "$HASH" "$MAC" )
+			SPEED_UPLOAD="32"
+			SPEED_DOWNLOAD="256"
+
+			_tunnel config_insert_new_client "$TUNNEL_ID" "$MAC" "$IP_USER" "$SPEED_UPLOAD" "$SPEED_DOWNLOAD"
+			_tunnel config_rebuild     >"/tmp/tunnel/vtun_server.conf"
+			_tunnel daemon_apply_config "/tmp/tunnel/vtun_server.conf"
+		else
+			echo "FALSE=;"
+		fi
+	;;
+	*)
+		echo "FALSE=;"
+	;;
+esac
