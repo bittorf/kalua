e99d728ed850038afe9ba3c661b08e7b371ad76c
Bastian Bittorf <bittorf@bluebottle.com>
2011-08-08 15:15:33 +0200

    364669 dhcp_del() splash the user again

--- www/cgi-bin-tunnel.sh
+++ www/cgi-bin-tunnel.sh
@@ -13,21 +13,23 @@ _log do tunnel_helper daemon info "ACTION: $ACTION QUERY: $QUERY_STRING"
 case "$ACTION" in
 	tunnel_possible)
 		if _tunnel check_local_capable ; then
+
 			_watch counter "/tmp/tunnel_id" increment 1 max 65 || {		# fixme! set to 0 during nightly/kick_user_all()
 				echo "FALSE=;"
 				exit
 			}
-
 			read TUNNEL_ID <"/tmp/tunnel_id"
-
-			echo -n "TRUE=;"
-			echo -n "TUNNEL_IP_CLIENT=$( _tunnel id2ip $TUNNEL_ID client );"
-			echo -n "TUNNEL_IP_SERVER=$( _tunnel id2ip $TUNNEL_ID server );"
-			echo    "TUNNEL_MASK=30;"
 
 	#		eval $( _tunnel get_speed_for_hash "$HASH" "$MAC" )
 			SPEED_UPLOAD="32"
 			SPEED_DOWNLOAD="256"
+
+			echo -n "TRUE=;"
+			echo -n "SPEED_UPLOAD=$SPEED_UPLOAD;"
+			echo -n "SPEED_DOWNLOAD=$SPEED_DOWNLOAD;"
+			echo -n "TUNNEL_IP_CLIENT=$( _tunnel id2ip $TUNNEL_ID client );"
+			echo -n "TUNNEL_IP_SERVER=$( _tunnel id2ip $TUNNEL_ID server );"
+			echo    "TUNNEL_MASK=30;"
 
 			_tunnel config_insert_new_client "$TUNNEL_ID" "$MAC" "$IP_USER" "$SPEED_UPLOAD" "$SPEED_DOWNLOAD"
 			_tunnel config_rebuild     >"/tmp/tunnel/vtun_server.conf"
