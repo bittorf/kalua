8644e9bb4f79c0cbc56e8c2355a8676cc5bd4abe
Bastian Bittorf <bittorf@bluebottle.com>
2011-10-31 20:48:55 +0100

    note, where to find the configs

--- openwrt-config/hardware/strip_config.sh
+++ openwrt-config/hardware/strip_config.sh
@@ -9,3 +9,10 @@ FILE="$1"
 }
 
 sed -i -n '/^[^#]/p' "$FILE"
+
+# kernel.config
+# ar71xx:   ./build_dir/linux-ar71xx_generic/linux-2.6.39.4/.config
+# brcm47xx: ./build_dir/linux-brcm47xx/linux-3.0.3/.config
+#
+# openwrt.config
+# .config
