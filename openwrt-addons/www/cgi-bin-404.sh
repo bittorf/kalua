552e79f1e6b03918d5acaad63cb0538dcadabd23
Bastian Bittorf <bittorf@bluebottle.com>
2012-02-21 11:47:26 +0100

    kalua: cgi-bin-404.sh: add *youtube* USER_AGENT (lowercase) to error403-list

--- openwrt-addons/www/cgi-bin-404.sh
+++ openwrt-addons/www/cgi-bin-404.sh
@@ -13,7 +13,7 @@ case "$HTTP_USER_AGENT" in
 	"CaptiveNetworkSupport"*)	# apple captive portel checker -> _http spoof_captive_portal_checker_apple
 		ERROR=403
 	;;
-	*"YouTube"*|"WifiHotspot"|"Skype WISPr"|*"Apple-PubSub"*|*"XProtectUpdater"*|"MPlayer"*|"Microsoft-CryptoAPI"*|"WinHttp-Autoproxy-Service"*|"Windows-Update-Agent"*)
+	*"youtube"*|*"YouTube"*|"WifiHotspot"|"Skype WISPr"|*"Apple-PubSub"*|*"XProtectUpdater"*|"MPlayer"*|"Microsoft-CryptoAPI"*|"WinHttp-Autoproxy-Service"*|"Windows-Update-Agent"*)
 		ERROR=403
 	;;
 esac
