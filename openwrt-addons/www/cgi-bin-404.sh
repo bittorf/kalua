d33e61c168ec200e1822a29707a40e660a856373
Bastian Bittorf <bittorf@bluebottle.com>
2012-02-27 15:34:44 +0100

    kalua: cgi-bin-404.sh: add 'Microsoft BITS' to forbidden HTTP_USER_AGENT

--- openwrt-addons/www/cgi-bin-404.sh
+++ openwrt-addons/www/cgi-bin-404.sh
@@ -13,7 +13,7 @@ case "$HTTP_USER_AGENT" in
 	"CaptiveNetworkSupport"*)	# apple captive portel checker -> _http spoof_captive_portal_checker_apple
 		ERROR=403
 	;;
-	"ZoneAlarm"*|*"youtube"*|*"YouTube"*|"WifiHotspot"|"Skype WISPr"|*"Apple-PubSub"*|*"XProtectUpdater"*|"MPlayer"*|"Microsoft-CryptoAPI"*|"WinHttp-Autoproxy-Service"*|"Windows-Update-Agent"*)
+	"Microsoft BITS"*|"ZoneAlarm"*|*"youtube"*|*"YouTube"*|"WifiHotspot"|"Skype WISPr"|*"Apple-PubSub"*|*"XProtectUpdater"*|"MPlayer"*|"Microsoft-CryptoAPI"*|"WinHttp-Autoproxy-Service"*|"Windows-Update-Agent"*)
 		ERROR=403
 	;;
 esac
