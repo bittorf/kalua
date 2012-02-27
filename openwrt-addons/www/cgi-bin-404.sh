5e6c755bd0ef18fefa08c94073a383ace9de7291
Bastian Bittorf <bittorf@bluebottle.com>
2012-02-27 15:37:10 +0100

    kalua: cgi-bin-404.sh: add 'htcUPCTLoader' to forbidden HTTP_USER_AGENT

--- openwrt-addons/www/cgi-bin-404.sh
+++ openwrt-addons/www/cgi-bin-404.sh
@@ -13,7 +13,7 @@ case "$HTTP_USER_AGENT" in
 	"CaptiveNetworkSupport"*)	# apple captive portel checker -> _http spoof_captive_portal_checker_apple
 		ERROR=403
 	;;
-	"Microsoft BITS"*|"ZoneAlarm"*|*"youtube"*|*"YouTube"*|"WifiHotspot"|"Skype WISPr"|*"Apple-PubSub"*|*"XProtectUpdater"*|"MPlayer"*|"Microsoft-CryptoAPI"*|"WinHttp-Autoproxy-Service"*|"Windows-Update-Agent"*)
+	"htcUPCTLoader"|"Microsoft BITS"*|"ZoneAlarm"*|*"youtube"*|*"YouTube"*|"WifiHotspot"|"Skype WISPr"|*"Apple-PubSub"*|*"XProtectUpdater"*|"MPlayer"*|"Microsoft-CryptoAPI"*|"WinHttp-Autoproxy-Service"*|"Windows-Update-Agent"*)
 		ERROR=403
 	;;
 esac
