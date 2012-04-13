1869cbcdef9cd8464efbe12ee3f7d57e2b3b300f
Bastian Bittorf <bittorf@bluebottle.com>
2012-04-13 13:04:03 +0200

    kalua: /www/cgi-bin-collect.sh adding local timestamp and using speedcode now/no external deps 2/2 (typo)

--- openwrt-addons/www/cgi-bin-collect.sh
+++ openwrt-addons/www/cgi-bin-collect.sh
@@ -2,4 +2,4 @@
 
 read UPTIME REST <"/proc/uptime"; UPTIME="${UPTIME%.*}"
 echo "UPTIME=${UPTIME}&REMOTE_ADDR=${REMOTE_ADDR}&$QUERY_STRING" >>"/tmp/COLLECT_DATA"
-echo -en "Content-type: test/plain\n\nOK"
+echo -en "Content-type: text/plain\n\nOK"
