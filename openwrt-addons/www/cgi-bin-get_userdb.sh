6376c3b8953e8bbf7d9da02830da7ead224bbf12
Bastian Bittorf <bittorf@bluebottle.com>
2012-10-08 14:22:30 +0200

    kalua: append '# OK' to copy of userdb and check after download

--- openwrt-addons/www/cgi-bin-get_userdb.sh
+++ openwrt-addons/www/cgi-bin-get_userdb.sh
@@ -9,7 +9,11 @@ eval $( _ipsystem do "$NODE" | grep "[N|I]ADR=" )
 
 case "$REMOTE_ADDR" in
 	$WANADR|$LANADR|$WIFIADR)
-		cat /tmp/DB/USER/login/meta_index || echo "# ERROR: could not read"
+		if cat /tmp/DB/USER/login/meta_index; then
+			echo "# OK"
+		else
+			echo "# ERROR: could not read"
+		fi
 	;;
 	*)
 		echo "# ERROR: Download from '$REMOTE_ADDR' not allowed"
