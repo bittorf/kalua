33f40ca12876a89a8e320308aecac00a9315f6f6
andibraeu <ab@andi95.de>
2012-12-02 00:29:56 +0100

    kalua: added patch_load_kmodules.sh

---
+++ openwrt-addons/usr/sbin/cron.patch_load_kmodules.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+modules_blacklist()
+{
+	echo -n "ipt_REJECT|nf_nat_ftp|nf_nat_irc|nf_conntrack_irc|nf_conntrack_ftp|nls_base|arc4|crypto_algapi|ipt_ULOG|xt_state"
+}
+
+modules_whitelist()
+{
+	echo -n "diag|switch-*"		# are needed, but can be unloaded after netifd-init
+}
+
+modules_allowed()
+{
+	local line
+
+	while read line; do {
+		set -- $line
+		echo -n "${1}*|"
+	} done </proc/modules
+}
+
+output_new_function()
+{
+	echo
+	echo "load_modules()	# patched from $0"
+	echo "{"
+	echo "	local line file t1 t2 duration trash list_unload_later list_reverse kmodule"
+	echo
+	echo "	read t1 trash </proc/uptime"
+	echo
+	echo '	while [ -n "$1" ]; do {'
+	echo '		file="$1"'
+	echo "		shift"
+	echo
+	echo '		line="$( cat "$file" )"'
+	echo '		test ${#line} -eq 0 && return'
+	echo
+	echo "		while read line; do {"
+	echo '			case "$line" in'
+	echo "				$( modules_blacklist ))"
+	echo '					echo >>/tmp/KMODULE.action "# $line"'
+	echo '				;;'
+	echo "				$( modules_allowed )$( modules_whitelist ))"
+	echo '					case "$line" in'
+	echo "						$( modules_whitelist ))"
+	echo '							# without parameters'
+	echo '							echo >>/tmp/KMODULE.action "# + insmod $line - (unload later!)"'
+	echo '							list_unload_later="$list_unload_later ${line/ */}"'
+	echo '						;;'
+	echo '						*)'
+	echo '							echo >>/tmp/KMODULE.action "# + insmod $line"'
+	echo '							insmod $line'
+	echo '						;;'
+	echo '					esac'
+	echo "				;;"
+	echo "				*)"
+	echo '					echo >>/tmp/KMODULE.action "# $line"'
+	echo "				;;"
+	echo "			esac"
+	echo '		} done <"$file"'
+	echo "	} done"
+	echo
+	echo '	for kmodule in $list_unload_later; do list_reverse="$kmodule $list_reverse"; done'
+	echo '	for kmodule in $list_reverse "diag"; do {'
+	echo '		echo >>/tmp/KMODULE.action "rmmod $kmodule"'
+	echo '	} done'
+	echo
+	echo '	read t2 trash </proc/uptime; duration=$(( ${t2//./} - ${t1//./} ))'
+	echo '	echo >>/tmp/KMODULE.action "# done in $(( $duration / 100 )).$(( $duration % 100 )) sec"'
+	echo "}"
+}
+
+grep -q ^"load_modules()	# patched" "/etc/functions.sh" || {
+	output_new_function >>"/etc/functions.sh"
+}
