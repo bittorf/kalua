1b64cb74a17305ce303eca704e041c4b4e42695e
Bastian Bittorf <bittorf@bluebottle.com>
2012-12-01 23:29:43 +0100

    kalua: moved generating of /tmp/weblogin_cached_for_overload to 404-page

--- openwrt-addons/www/cgi-bin-404.sh
+++ openwrt-addons/www/cgi-bin-404.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-[ -e "/tmp/service_ssh_nowatching" ] && {
+# [ -e "/tmp/service_ssh_nowatching" ] && {
+	[ -e "/tmp/weblogin_cached_for_overload" ] || {
+		export QUERY_STRING="REDIRECTED=1"
+		/www/cgi-bin-welcome.sh >/dev/null
+	}
+
 	cat "/tmp/weblogin_cached_for_overload"
 	exit 0
-}
 
 ERROR=302
 
