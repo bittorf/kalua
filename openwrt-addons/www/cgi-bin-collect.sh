a144b32bebce6093a5883de46e78e240900029ff
Bastian Bittorf <bittorf@bluebottle.com>
2013-05-28 14:06:59 +0200

    kalua: www/cgi-bin-collect.sh: HTTP-Header now correct, first MAC was always trash

--- openwrt-addons/www/cgi-bin-collect.sh
+++ openwrt-addons/www/cgi-bin-collect.sh
@@ -2,7 +2,7 @@
 
 read UPTIME REST <"/proc/uptime"; UPTIME="${UPTIME%.*}"
 echo "UPTIME=${UPTIME}&REMOTE_ADDR=${REMOTE_ADDR}&$QUERY_STRING" >>"/tmp/COLLECT_DATA"
-echo -en "Content-type: text/plain\n\nOK "
+echo -en "Content-type: text/plain\n\n"
 
 # report which mac's are ok to allow (e.g. DHCP)
 ls -1 /tmp/vds_user_* 2>/dev/null | cut -d'_' -f3
