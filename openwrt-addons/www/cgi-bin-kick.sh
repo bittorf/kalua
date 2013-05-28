366145d2de06e37bf46f0ada86b2a968337001c5
Bastian Bittorf <bittorf@bluebottle.com>
2013-05-28 14:04:35 +0200

    kalua: www/cgi-bin-kick.sh: always output correct HTTP-Header, even when router ignores the message

--- openwrt-addons/www/cgi-bin-kick.sh
+++ openwrt-addons/www/cgi-bin-kick.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 . /tmp/loader
 
+echo -en "Content-type: text/plain\n\nOK"
+
 eval $( _http query_string_sanitize )	# CAH|CMA
 
 _netfilter user_probe "$CMA" || exit 1
@@ -11,5 +13,3 @@ read HASH <"/tmp/vds_user_$CMA"
 [ "$HASH" = "$CAH" ] || exit
 
 _scheduler add "_netfilter user_del $CMA"
-
-echo -en "Content-type: text/plain\n\nOK"
