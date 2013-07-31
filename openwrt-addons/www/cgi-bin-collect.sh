0f492b08e761c91d4a6753fe787e2e91204c1a78
andibraeu <ab@andi95.de>
2013-07-31 17:47:17 +0200

    kalua: removed ballast

--- openwrt-addons/www/cgi-bin-collect.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-read UPTIME REST <"/proc/uptime"; UPTIME="${UPTIME%.*}"
-echo "UPTIME=${UPTIME}&REMOTE_ADDR=${REMOTE_ADDR}&$QUERY_STRING" >>"/tmp/COLLECT_DATA"
-echo -en "Content-type: text/plain\n\nOK "
-
-# report which mac's are ok to allow (e.g. DHCP)
-ls -1 /tmp/vds_user_* 2>/dev/null | cut -d'_' -f3
