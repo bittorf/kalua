0311aa5ec5c736a266ab7430c8ac16bc5820e0ce
Bastian Bittorf <bittorf@bluebottle.com>
2013-09-29 22:21:41 +0200

    kalua: www/cgi-bin-get_userdb.sh: hack for making sure, that only real nodes (OLSR/Mid) can make a download

--- openwrt-addons/www/cgi-bin-get_userdb.sh
+++ openwrt-addons/www/cgi-bin-get_userdb.sh
@@ -7,23 +7,31 @@ _ipsystem include
 NODE="$( _ipsystem do "$REMOTE_ADDR" )"
 eval $( _ipsystem do "$NODE" | grep "[N|I]ADR=" )
 
-case "$REMOTE_ADDR" in
-	# fixme!
-	$WANADR|$LANADR|$WIFIADR|192.168.*)
-		# why tac? it's likely, that we grep for a new login - this should match faster
-		# this is >1 magnitude faster than sed-tac
+# ip route | fgrep "10.63.6.3 via "
 
-		if [ -e "/tmp/FREE" ]; then
-			echo "# OK - FREE"
-		else
-			if grep -n '' /tmp/DB/USER/login/meta_index | sort -rn | cut -d: -f2- ; then
-				echo "# OK"
-			else
-				echo "# ERROR: could not read"
-			fi
-		fi
+case "$REMOTE_ADDR" in
+	192.168.*)
+		# fixme!
+	;;
+	$WANADR|$LANADR|$WIFIADR)
 	;;
 	*)
-		echo "# ERROR: Download from '$REMOTE_ADDR' not allowed"
+		# simple way for ensure, that only real nodes (OLSR/Mid) can do this
+		ip route | fgrep -q "$REMOTE_ADDR via " || {
+			echo "# ERROR: Download from '$REMOTE_ADDR' not allowed"
+			exit 0
+		}
 	;;
 esac
+
+if [ -e "/tmp/FREE" ]; then
+	echo "# OK - FREE"
+else
+	# why tac? it's likely, that we grep for a new login - this should match faster
+	# this is >1 magnitude faster than sed-tac
+	if grep -n '' /tmp/DB/USER/login/meta_index | sort -rn | cut -d: -f2- ; then
+		echo "# OK"
+	else
+		echo "# ERROR: could not read"
+	fi
+fi
