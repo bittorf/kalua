2fa889f1e3a516d9c3650d81ccc2ed82f3405d75
Bastian Bittorf <bittorf@bluebottle.com>
2013-10-02 22:46:47 +0200

    kalua: www/cgi-bin-get_userdb.sh: output correct http-header in all cases

--- openwrt-addons/www/cgi-bin-get_userdb.sh
+++ openwrt-addons/www/cgi-bin-get_userdb.sh
@@ -19,6 +19,7 @@ case "$REMOTE_ADDR" in
 	*)
 		# simple way for ensure, that only real nodes (OLSR/Mid) can do this
 		ip route | fgrep -q "$REMOTE_ADDR via " || {
+			_http header_mimetype_output text/plain
 			echo "# ERROR: Download from '$REMOTE_ADDR' not allowed"
 			exit 0
 		}
@@ -26,6 +27,7 @@ case "$REMOTE_ADDR" in
 esac
 
 if [ -e "/tmp/FREE" ]; then
+	_http header_mimetype_output text/plain
 	echo "# OK - FREE"
 else
 	# fixme! must be updated if something changes
