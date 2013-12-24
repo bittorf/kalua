84c1ae849439ad04cefb4b90cae95ab8ec31ff13
Bastian Bittorf <bittorf@bluebottle.com>
2013-12-24 11:47:58 +0100

    kalua: scheduler: incoming_wifi_activity() for new hybrid-devices wlan0-1/wlan0 can change, so better check which device we measure (we want the adhoc-device)

--- openwrt-addons/www/cgi-bin-collect.sh
+++ openwrt-addons/www/cgi-bin-collect.sh
@@ -3,6 +3,7 @@
 [ -n "$QUERY_STRING" ] && {
 	read UPTIME REST <"/proc/uptime"; UPTIME="${UPTIME%.*}"
 	echo "UPTIME=${UPTIME}&REMOTE_ADDR=${REMOTE_ADDR}&$QUERY_STRING" >>"/tmp/COLLECT_DATA"
+	# processed later via '/usr/sbin/cron.add_collected_userdata_into_db'
 }
 
 echo -en "Content-type: text/plain\n\n"
