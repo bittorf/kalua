21d13457b706f8bf6a6db15aa4dcf3155002b2f3
Bastian Bittorf <bittorf@bluebottle.com>
2014-02-12 20:45:34 +0100

    kalua: hotplug: prevent DNS-aplification attac on WAN if needed via netfilter rules

--- openwrt-build/build.sh
+++ openwrt-build/build.sh
@@ -33,7 +33,6 @@ cat >/dev/null <<EOL
 list_options()
 {
 	echo 'Standard'					# sizecheck
-	echo 'Standard,kalua'				# valid for community
 	echo 'Standard,VDS,kalua'			# typical hotel
 	echo 'Standard,VDS,USBprinter,kalua'		# roehr30
 	echo 'Standard,VDS,BTCminerBFL,kalua'		# ejbw/16
@@ -44,6 +43,16 @@ list_options()
 	echo 'Small,OLSRd,kalua'
 	echo 'Small,BatmanAdv,kalua'
 	echo 'Small,OLSRd,BatmanAdv,kalua'
+	echo 'Small,VDS,OLSRd,kalua'
+	echo 'Small,VDS,BatmanAdv,kalua'
+	echo 'Small,VDS,OLSRd,BatmanAdv,kalua'
+	echo 'Small,noPPPoE'				# sizecheck
+	echo 'Small,noPPPoE,OLSRd,kalua'
+	echo 'Small,noPPPoE,BatmanAdv,kalua'
+	echo 'Small,noPPPoE,OLSRd,BatmanAdv,kalua'
+	echo 'Small,noPPPoE,VDS,OLSRd,kalua'
+	echo 'Small,noPPPoE,VDS,BatmanAdv,kalua'
+	echo 'Small,noPPPoE,VDS,OLSRd,BatmanAdv,kalua'
 	echo 'Mini'
 
 	# + alles mit LuCIfull
@@ -56,11 +65,10 @@ list_hw()
 	kalua/openwrt-build/build.sh --hardware list plain
 }
 
-for HW in $( list_hw ); do {
-	kalua/openwrt-build/build.sh --hardware "$HW" \
-				     --option "Standard,VDS,kalua" \
-				     --openwrt "r39455" \
-				     --release 'stable'
+for OPT in $( list_options ); do {
+	for HW in $( list_hw ); do {
+		kalua/openwrt-build/build.sh --hardware "$HW" --option "$OPT" --openwrt "r39455" --release 'testing'
+	} done
 } done
 EOL
 
