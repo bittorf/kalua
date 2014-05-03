781f4a79938d4a5b947e26f99e6a4b7fd0f4a39c
andibraeu <ab@andi95.de>
2014-05-03 13:43:54 +0200

    preparation for changing vpn connect process

--- vpn.sh
+++ vpn.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+. /tmp/loader
 
 # load json lib
 . /usr/share/libubox/jshn.sh
@@ -8,29 +9,52 @@ DOMAIN="weimarnetz.de"
 #vpn prefix
 PREFIX="vpn"
 #json info url
-JSONPATH="/freifunk/vpn/vpn.json"
+JSONPATH="/freifunk/vpn/vpn.php"
 
+#temporary number for comparisons
+CLIENTS=12345
 vpnNr=1
 SERVER=$PREFIX$vpnNr.$DOMAIN
-vpnServerResponse="$( ping -q -c 1 -W 1 $SERVER |grep round-trip|cut -f 4 -d ' '|cut -f 0 -d '/'|sed -e 's/\.//g' )"
-vpnServerName=$SERVER
+vpnServerResponse="$( ping -q -c 1 -W 1 $SERVER 2>/dev/null |grep round-trip|cut -f 4 -d ' '|cut -f 0 -d '/'|sed -e 's/\.//g' )"
+if [ -n $responseTime  ];
+then
+	URL="http://$SERVER$JSONPATH"
+	eval $( jshn -r "$( _wget do "$URL" 1 2>/dev/null )" 2>/dev/null )
+	if [ -n "$JSON_VAR_clients" ];
+	then
+		CLIENTS=${JSON_VAR_clients}
+		PORT=${JSON_VAR_port_vtun_nossl_nolzo}
+		MTU=${JSON_VAR_maxmtu}
+		vpnServerName=$SERVER
+	fi
+fi
 
 for vpnNr in 2 3 4 5 6 7 8 9 10
 do
 	SERVER=$PREFIX$vpnNr.$DOMAIN
-	responseTime="$( ping -q -c 1 -W 1 $SERVER |grep round-trip|cut -f 4 -d " "|cut -f 0 -d "/"|sed -e 's/\.//g' )"
-	if [ $responseTime -lt $vpnServerResponse  ];
+	responseTime="$( ping -q -c 1 -W 1 $SERVER 2>/dev/null |grep round-trip|cut -f 4 -d " "|cut -f 0 -d "/"|sed -e 's/\.//g' )"
+	if [ -n $responseTime  ];
 	then
-		vpnServerResponse=$responseTime
-		vpnServerName="$SERVER"
+		URL="http://$SERVER$JSONPATH"
+		eval $( jshn -r "$( _wget do "$URL" 1 2>/dev/null )" 2>/dev/null )
+		if  test 2>/dev/null "$JSON_VAR_clients" -lt "$CLIENTS" ;
+		then
+			CLIENTS=${JSON_VAR_clients}
+			PORT=${JSON_VAR_port_vtun_nossl_nolzo}
+			MTU=${JSON_VAR_maxmtu}
+			vpnServerResponse=$responseTime
+			vpnServerName="$SERVER"
+		fi
 	fi
 done
 
-URL="http://$vpnServerName$JSONPATH"
-echo $URL
-eval $( jshn -r "$( wget -qO - "$URL" )" )
+if [[ $CLIENTS = 12345 ]];
+then
+	CLIENTS=
+fi
 
-echo "Server: $JSON_VAR_server"
-echo "vtun_nossl_nolzo: $JSON_VAR_port_vtun_nossl_nolzo"
+echo "Server: $vpnServerName"
+echo "vtun_nossl_nolzo: $PORT"
+echo "clients: $CLIENTS"
 echo "vtun_ssl_lzo: $JSON_VAR_port_vtun_ssl_lzo"
-echo "maxmtu: $JSON_VAR_maxmtu"
+echo "maxmtu: $MTU"
