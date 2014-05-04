ad88a38146d857ac27b4933274196243cc4907b9
andibraeu <ab@andi95.de>
2014-05-04 22:33:43 +0200

    format

--- vpn.sh
+++ vpn.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-. /tmp/loader
-
 # load json lib
 . /usr/share/libubox/jshn.sh
 
@@ -16,25 +14,22 @@ CLIENTS=12345
 vpnNr=1
 SERVER=$PREFIX$vpnNr.$DOMAIN
 vpnServerResponse="$( ping -q -c 1 -W 1 $SERVER 2>/dev/null |grep round-trip|cut -f 4 -d ' '|cut -f 0 -d '/'|sed -e 's/\.//g' )"
-if [ -n $responseTime  ];
-then
+[ -n "$responseTime" ] && {
 	URL="http://$SERVER$JSONPATH"
 	eval $( jshn -r "$( _wget do "$URL" 1 2>/dev/null )" 2>/dev/null )
-	if [ -n "$JSON_VAR_clients" ];
-	then
+	[ -n "$JSON_VAR_clients" ] && {
 		CLIENTS=${JSON_VAR_clients}
 		PORT=${JSON_VAR_port_vtun_nossl_nolzo}
 		MTU=${JSON_VAR_maxmtu}
 		vpnServerName=$SERVER
-	fi
-fi
+  }
+}
 
 for vpnNr in 2 3 4 5 6 7 8 9 10
 do
 	SERVER=$PREFIX$vpnNr.$DOMAIN
 	responseTime="$( ping -q -c 1 -W 1 $SERVER 2>/dev/null |grep round-trip|cut -f 4 -d " "|cut -f 0 -d "/"|sed -e 's/\.//g' )"
-	if [ -n $responseTime  ];
-	then
+	[ -n $responseTime  ] && {
 		URL="http://$SERVER$JSONPATH"
 		eval $( jshn -r "$( _wget do "$URL" 1 2>/dev/null )" 2>/dev/null )
 		if  test 2>/dev/null "$JSON_VAR_clients" -lt "$CLIENTS" ;
@@ -45,14 +40,12 @@ do
 			vpnServerResponse=$responseTime
 			vpnServerName="$SERVER"
 		fi
-	fi
+  }
 done
 
-if [[ $CLIENTS = 12345 ]];
-then
+[[ $CLIENTS = 12345 ]] && {
 	CLIENTS=
-fi
-
+}
 echo "Server: $vpnServerName"
 echo "vtun_nossl_nolzo: $PORT"
 echo "clients: $CLIENTS"
