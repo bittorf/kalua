12f634f9dd5062a612b01ffd8c95b653f888b718
Bastian Bittorf <bittorf@bluebottle.com>
2014-09-20 16:26:21 +0200

    kalua: www/cgi-bin-status.sh: show download-speed from neighbour 3/3

--- openwrt-addons/www/cgi-bin-status.sh
+++ openwrt-addons/www/cgi-bin-status.sh
@@ -384,11 +384,12 @@ EOF
 		echo " <td align='right'><small>$count</small></td>"
 		echo " <td sorttable_customkey='$octet3'> <a href='http://$neigh/cgi-bin-status.html'>$neigh</a> </td>"
 		echo " <td> <a href='http://$neigh/cgi-bin-status.html'>$remote_hostname</a> </td>"
-		echo " <td colspan='5' nowrap> vermisst, zuletzt gesehen vor $age </td>"
+		echo " <td colspan='5' nowrap align='right'> vermisst seit $age </td>"
 		echo " <td align='right' title='$cost_best_time'> $cost_best </td>"
-		echo " <td>&nbsp;</td>"
+		echo " <td>&nbsp;</td>"		# speed
+		echo " <td>&nbsp;</td>"		# SNR
 		echo " <td align='center'> ${metric:-&mdash;} </td>"
-		echo " <td colspan='4'> &nbsp; </td>"
+		echo " <td colspan='3'> &nbsp; </td>"
 		echo "</tr>"
 	} done
 }
