ae2e3bfc211ac0644786722c7a053b1611808ae0
Bastian Bittorf <bittorf@bluebottle.com>
2014-10-20 10:27:30 +0200

    build: add openwrt-build/smoketest.sh - it can build _all_ openwrt-architectures or a subset for testing

---
+++ openwrt-build/smoketest.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+TYPE="$1"
+OPTION="$2"
+MYLOG='special_log.txt'
+
+[ -z "$TYPE" ] && {
+	echo "Usage: $0 <type> <architecture>"
+	echo "       type can be 'kernel' or 'full'"
+	echo "       with you can select the kernel-version of which arch's are taken, e.g. '3\.10\.'"
+
+	exit 1
+}
+
+log()
+{
+	logger -s "$0: $1"
+	echo "[$( date )] $0: $1" >>"$MYLOG"
+}
+
+[ -e 'Config.in' ] || {
+	git clone git://nbd.name/openwrt.git
+	cd openwrt
+}
+
+if [ "$TYPE" = 'full' ]; then
+	MAKECOMMAND="-j25"
+else
+	MAKECOMMAND="-j25 target/linux/compile"
+fi
+
+list_architectures()
+{
+	local with_kernel="$1"		# empty = all
+
+	# target/linux/gemini/Makefile:LINUX_VERSION:=3.10.49 -> gemini
+	grep ^"LINUX_VERSION:=${with_kernel}" target/linux/*/Makefile | cut -d'/' -f3
+}
+
+for ARCH in $( list_architectures "$OPTION" ); do {
+	log "$ARCH - start"
+	echo "CONFIG_TARGET_${ARCH}=y" >'.config' && make defconfig
+
+	if make $MAKECOMMAND; then
+		log "$ARCH - OK"
+
+		for DIR in bin build_dir staging_dir; do {
+			du -sh "$DIR" && rm -fR "$DIR"
+		} done
+	else
+		log "$ARCH - ERROR"
+	fi
+} done
