b78ebf7277c31e60aaff37ece9184b89b4c75f48
Bastian Bittorf <bittorf@bluebottle.com>
2015-06-05 16:12:45 +0200

    kalua: net: roaming: implement roaming_querymac() and roaming_getlist()

--- openwrt-addons/www/cgi-bin-collect.sh
+++ openwrt-addons/www/cgi-bin-collect.sh
@@ -11,7 +11,28 @@ case "$QUERY_STRING" in
 		echo 'OK - empty QUERY'
 	;;
 	*'roaming_add'*)
+		. /tmp/loader
+		eval $( _http query_string_sanitize )
+
+		# see: net_roaming_report_new()
+		echo "$mac $ip $expires" >>'/tmp/roaming'
 		echo 'OK'
+	;;
+	*'roaming_getlist'*)
+		cat '/tmp/roaming'
+		echo 'OK'
+	;;
+	*'roaming_querymac'*)
+		. /tmp/loader
+		eval $( _http query_string_sanitize )
+
+		# format: mac ip expires - see: net_roaming_report_new()
+		if LINE="$( grep -s ^"$mac" '/tmp/roaming' )"; then
+			set -- $LINE
+			echo $2
+		else
+			_log do roaming_querymac daemon info "no entry for $mac"
+		fi
 	;;
 	*)
 		# processed later via '/usr/sbin/cron.add_collected_userdata_into_db'
