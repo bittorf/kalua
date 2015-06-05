e04decdb93d406f2b0c48d13d4736d1d6eb10119
Bastian Bittorf <bittorf@bluebottle.com>
2015-06-05 15:27:55 +0200

    kalua: net: roaming_report_new() first draft for netwide reporting of new roaming mac/ip tupel

--- openwrt-addons/www/cgi-bin-collect.sh
+++ openwrt-addons/www/cgi-bin-collect.sh
@@ -1,16 +1,26 @@
 #!/bin/sh
 
-# is called when this is the 'weblogin_authserver'
+# this script is called when this is the 'weblogin_authserver'
 # from any node in the network via netfilter_user_stats_process()
-
-[ -n "$QUERY_STRING" ] && {
-	read UPTIME REST <"/proc/uptime"; UPTIME="${UPTIME%.*}"
-	echo "UPTIME=${UPTIME}&REMOTE_ADDR=${REMOTE_ADDR}&$QUERY_STRING" >>"/tmp/COLLECT_DATA"
-	# processed later via '/usr/sbin/cron.add_collected_userdata_into_db'
-}
+# or from net_roaming_report_new()
 
 echo -en "Content-type: text/plain\n\n"
 
-# report which mac's are ok to allow (e.g. DHCP)
-ls -1 /tmp/vds_user_* 2>/dev/null | cut -d'_' -f3
-echo "OK-UNIXTIME=$( date +%s )"
+case "$QUERY_STRING" in
+	'')
+		echo 'OK - empty QUERY'
+	;;
+	*'roaming_add'*)
+		echo 'OK'
+	;;
+	*)
+		# processed later via '/usr/sbin/cron.add_collected_userdata_into_db'
+		read UPTIME REST <'/proc/uptime'
+		echo "UPTIME=${UPTIME%.*}&REMOTE_ADDR=${REMOTE_ADDR}&$QUERY_STRING" >>'/tmp/COLLECT_DATA'
+
+		# while we have a conversation anyway,
+		# report which mac's are ok to allow (e.g. DHCP)
+		ls -1 /tmp/vds_user_* 2>/dev/null | cut -d'_' -f3
+		echo "OK-UNIXTIME=$( date +%s )"
+	;;
+esac
