63da63bbf16185c5b443606535d696de1d46cb5b
Bastian Bittorf <bittorf@bluebottle.com>
2015-06-06 10:27:27 +0200

    kalua: cron.wireless_events: only ask roaming-server once

--- openwrt-addons/www/cgi-bin-collect.sh
+++ openwrt-addons/www/cgi-bin-collect.sh
@@ -31,7 +31,7 @@ case "$QUERY_STRING" in
 			set -- $LINE
 			echo $2
 		else
-			_log do roaming_querymac daemon info "no entry for $mac"
+			_log do roaming_querymac daemon info "no entry for $mac ($REMOTE_ADDR asked)"
 		fi
 	;;
 	*)
