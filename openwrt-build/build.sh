e30e0621efa604d98af6e811a11a67136a6c3a98
Bastian Bittorf <bittorf@bluebottle.com>
2015-06-23 12:14:35 +0200

    build: travis: fix downloadlink for shellcheck binary

--- openwrt-build/build.sh
+++ openwrt-build/build.sh
@@ -2528,7 +2528,7 @@ unittest_do()
 		_system load || return 1
 
 		[ -n "$TRAVIS" ] && {
-			wget -O 'shellsheck.deb' 'http://ftp.debian.org/debian/pool/main/s/shellcheck/shellcheck_0.3.5-2_amd64.deb'
+			wget -O 'shellsheck.deb' "http://ftp.debian.org/debian/pool/main/s/shellcheck/shellcheck_0.3.7-1_$( _system architecture ).deb"
 			sudo dpkg -i 'shellsheck.deb'
 		}
 
