5a640f9dc0cfa4187450af827e9b504d48a4ac08
Bastian Bittorf <bittorf@bluebottle.com>
2015-09-22 21:45:32 +0200

    kalua: profile.d: cleanup

--- openwrt-addons/etc/profile.d/kalua.sh
+++ openwrt-addons/etc/profile.d/kalua.sh
@@ -2,13 +2,13 @@
 
 export PS1='\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\] '
 
-alias myssh='set -x; ssh -i $( _ssh key_public_fingerprint_get keyfilename ); set +x'
-alias ll='ls -la'
-alias flush='echo flushing_caches; echo 3 > /proc/sys/vm/drop_caches'
-alias lr='logread'
-alias regen='/etc/kalua_init; LODEV= . /tmp/loader'
 alias n='wget -qO - http://127.0.0.1:2006/neighbours'
 alias n2='echo /nhdpinfo link | nc 127.0.0.1 2009'
+alias ll='ls -la'
+alias lr='logread'
+alias flush='_system ram_free flush'
+alias myssh='ssh -i $( _ssh key_public_fingerprint_get keyfilename )'
+alias regen='/etc/kalua_init; LODEV= . /tmp/loader'
 
-test -e '/tmp/loader' && . '/tmp/loader'
-echo "for some hints type: _help overview"
+[ -e '/tmp/loader' ] && . '/tmp/loader'
+echo 'for some hints type: _help overview'
