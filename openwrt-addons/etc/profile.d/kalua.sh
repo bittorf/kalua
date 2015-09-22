0583b99f37aa7ba6496bce17a49ece5893817afa
Bastian Bittorf <bittorf@bluebottle.com>
2015-09-22 21:38:47 +0200

    kalua: profile.d: cheat the 'shellcheck' test and avoid some code duplication with reusing _ssh_key_public_fingerprint_get()

--- openwrt-addons/etc/profile.d/kalua.sh
+++ openwrt-addons/etc/profile.d/kalua.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
-for PS1 in /etc/dropbear/dropbear_dss_host_key /etc/dropbear/dropbear_rsa_host_key /etc/ssh/ssh_host_rsa_key.pub; do {
-	[ -e "$PS1" ] && {
-		alias myssh="echo 'executing: ssh -i $PS1'; ssh -i $PS1"
-		break
-	}
-} done
-
 export PS1='\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\] '
 
+alias myssh='set -x; ssh -i $( _ssh key_public_fingerprint_get keyfilename ); set +x'
 alias ll='ls -la'
 alias flush='echo flushing_caches; echo 3 > /proc/sys/vm/drop_caches'
 alias lr='logread'
