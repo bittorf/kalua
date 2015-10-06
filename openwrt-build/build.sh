1e58e85a239be0c9b6264027ed709ae1b1198105
Bastian Bittorf <bittorf@bluebottle.com>
2015-10-06 13:01:27 +0200

    build: fix a shellcheck issues

--- openwrt-build/build.sh
+++ openwrt-build/build.sh
@@ -2893,24 +2893,24 @@ unittest_do()
 			# SC1091: Not following: /tmp/loader was not specified as input (see shellcheck -x).
 			# SC1090: Can't follow non-constant source. Use a directive to specify location.
 			#
+			# SC2016: echp '$a' => Expressions don't expand in single quotes, use double quotes for that.
+			# SC2029: ssh "$serv" "command '$server_dir'" => Note that, unescaped, this expands on the client side.
+			# SC2031: FIXME! ...in net_local_inet_offer()
 			# SC2039: In POSIX sh, echo flags are not supported.
 			#  SC2039: In POSIX sh, HOSTNAME is not supported.
 			#  SC2039: In POSIX sh, string replacement is not supported.
 			#  SC2039: In POSIX sh, 'let' is not supported.
-			# SC2155: local var="$( bla )" -> loosing returncode
-			#  - https://github.com/koalaman/shellcheck/issues/262
 			# SC2046: eval $( _http query_string_sanitize ) Quote this to prevent word splitting.
 			# SC2086: ${CONTENT_LENGTH:-0} Double quote to prevent globbing and word splitting.
 			#  - https://github.com/koalaman/shellcheck/issues/480#issuecomment-144514791
-			# SC2031: FIXME! ...in net_local_inet_offer()
-			# SC2016: echp '$a' => Expressions don't expand in single quotes, use double quotes for that.
-			# SC2029: ssh "$serv" "command '$server_dir'" => Note that, unescaped, this expands on the client side.
+			# SC2155: local var="$( bla )" -> loosing returncode
+			#  - https://github.com/koalaman/shellcheck/issues/262
 			# SC2166: Prefer [ p ] && [ q ] as [ p -a q ] is not well defined.
 
 			shellsheck_ignore()
 			{
 				printf 'SC1007,SC1010,SC1090,SC1091'
-				printf ',SC2046,SC2086,SC2088,SC2030,SC2031,SC2016,SC2029,SC2039,SC2155,SC2162,SC2166'
+				printf 'SC2016,SC2029,SC2031,SC2039,SC2046,SC2086,SC2155,SC2166'
 			}
 
 			log "testing with '$shellcheck_bin', ignoring: $( shellsheck_ignore )"
