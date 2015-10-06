7da8cf7eadc1f385c76b3e5a506e23257c41c9e9
Bastian Bittorf <bittorf@bluebottle.com>
2015-10-06 08:00:01 +0200

    kalua: fix SC2165 = This nested loop overrides the index variable of its parent

--- openwrt-build/build.sh
+++ openwrt-build/build.sh
@@ -2905,8 +2905,6 @@ unittest_do()
 			# SC2064: trap "command $var" => Use single quotes, otherwise this expands now rather than when signalled.
 			# SC2029: ssh "$serv" "command '$server_dir'" => Note that, unescaped, this expands on the client side.
 			# SC2166: Prefer [ p ] && [ q ] as [ p -a q ] is not well defined.
-#			# SC2165: This parent loop has its index variable overridden.
-#			#         This nested loop overrides the index variable of its parent.
 			# SC2119: Use logread "$@" if function's $1 should mean script's $1.
 
 			shellsheck_ignore()
