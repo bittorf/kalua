e043cf9e8d0839d6a73ee5b033df8f95b9704b3e
Bastian Bittorf <bittorf@bluebottle.com>
2015-10-09 21:02:25 +0200

    build: play with SC2016

--- openwrt-build/build.sh
+++ openwrt-build/build.sh
@@ -2939,7 +2939,7 @@ unittest_do()
 			# SC1091: Not following: /tmp/loader was not specified as input (see shellcheck -x).
 			# SC1090: Can't follow non-constant source. Use a directive to specify location.
 			#
-			# SC2016: echp '$a' => Expressions don't expand in single quotes, use double quotes for that.
+#			# SC2016: echp '$a' => Expressions don't expand in single quotes, use double quotes for that.
 			# SC2029: ssh "$serv" "command '$server_dir'" => Note that, unescaped, this expands on the client side.
 			# SC2031: FIXME! ...in net_local_inet_offer()
 			# SC2039: In POSIX sh, echo flags are not supported.
@@ -2956,7 +2956,7 @@ unittest_do()
 			shellsheck_ignore()
 			{
 				printf 'SC1007,SC1010,SC1090,SC1091,'
-				printf 'SC2016,SC2029,SC2031,SC2039,SC2046,SC2086,SC2155,SC2166'
+				printf 'SC2029,SC2031,SC2039,SC2046,SC2086,SC2155,SC2166'
 			}
 
 			log "testing with '$shellcheck_bin', ignoring: $( shellsheck_ignore )"
