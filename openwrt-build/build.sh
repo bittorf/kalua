fc86db3c307665a3b96e529fd6d0d1f209e50dcf
Bastian Bittorf <bittorf@bluebottle.com>
2015-10-12 16:28:42 +0200

    build: playing with SC2039

--- openwrt-build/build.sh
+++ openwrt-build/build.sh
@@ -2935,15 +2935,15 @@ unittest_do()
 			log "[OK] shellcheck not installed - no deeper tests"
 		else
 			$shellcheck_bin --version
-# WIP			# SC1007: Remove space after = if trying to assign a value (for empty string, use var='' ... ).
+			# SC1007: Remove space after = if trying to assign a value (for empty string, use var='' ... ).
 			# SC1010: "_log do ...' -> 'do' is a special keyword
 			# SC1091: Not following: /tmp/loader was not specified as input (see shellcheck -x).
 			# SC1090: Can't follow non-constant source. Use a directive to specify location.
 			#
-# WIP			# SC2016: echp '$a' => Expressions don't expand in single quotes, use double quotes for that.
+			# SC2016: echp '$a' => Expressions don't expand in single quotes, use double quotes for that.
 			# SC2029: ssh "$serv" "command '$server_dir'" => Note that, unescaped, this expands on the client side.
 			# SC2031: FIXME! ...in net_local_inet_offer()
-			# SC2039: In POSIX sh, echo flags are not supported.
+# WIP			# SC2039: In POSIX sh, echo flags are not supported.
 			#  SC2039: In POSIX sh, HOSTNAME is not supported.
 			#  SC2039: In POSIX sh, string replacement is not supported.
 			#  SC2039: In POSIX sh, 'let' is not supported.
@@ -2957,8 +2957,8 @@ unittest_do()
 
 			shellsheck_ignore()
 			{
-				printf 'SC1010,SC1090,SC1091,'
-				printf 'SC2029,SC2031,SC2039,SC2046,SC2086,SC2155,SC2166'
+				printf 'SC1007,SC1010,SC1090,SC1091,'
+				printf 'SC2016,SC2029,SC2031,SC2046,SC2086,SC2155,SC2166'
 			}
 
 			log "testing with '$shellcheck_bin', ignoring: $( shellsheck_ignore )"
