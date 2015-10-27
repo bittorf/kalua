f6e463a446fe5d44c1f634b471de168514fb3211
Bastian Bittorf <bittorf@bluebottle.com>
2015-10-27 13:42:14 +0100

    kalua: profile.d/kalua.sh: check and warn for non or weak root-password

--- openwrt-addons/etc/profile.d/kalua.sh
+++ openwrt-addons/etc/profile.d/kalua.sh
@@ -10,6 +10,18 @@ alias flush='_system ram_free flush'
 alias myssh='ssh -i $( _ssh key_public_fingerprint_get keyfilename )'
 alias regen='/etc/kalua_init; _(){ false;}; . /tmp/loader'
 
+case "$USER" in
+	'root')
+		grep -s ^"root:\$1\$b6usD77Q\$XPs6VECsQzFy9TUuQUAHW1:" '/etc/shadow' && {
+			echo "[ERROR] change weak root-password ('admin') with 'passwd'"
+		}
+
+		grep -s ^'root:$' '/etc/shadow' || {
+			echo "[ERROR] set root-password with 'passwd'"
+		}
+	;;
+esac
+
 _ t 2>/dev/null || {
 	[ -e '/tmp/loader' ] && {
 		. '/tmp/loader'
