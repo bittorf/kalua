6d7888a28548ded150213796c732b90f75b462ab
Bastian Bittorf <bittorf@bluebottle.com>
2016-05-09 15:06:50 +0200

    monitoring: tarball_update.sh: cleanup

--- openwrt-monitoring/tarball_update.sh
+++ openwrt-monitoring/tarball_update.sh
@@ -2,10 +2,24 @@
 
 NETWORK="$1"	# liszt28
 MODE="$2"	# testing
+TARBALL='/tmp/tarball.tgz'
 
 [ -z "$2" ] && {
 	echo "usage: $0 <network|all> <mode>"
 	echo "e.g. : $0 liszt28 testing"
+	exit 1
+}
+
+[ -e "$TARBALL" ] || {
+	cat <<EOF
+[ERROR] cannot find tarball '$TARBALL', please do:
+
+cd /root/tarball/
+cd kalua
+git pull
+cd ..
+kalua/openwrt-build/mybuild.sh build_kalua_update_tarball
+EOF
 	exit 1
 }
 
@@ -23,6 +37,9 @@ list_networks()
 
 for NW in $NETWORK; do {
 	DIR="/var/www/networks/$NW/tarball/$MODE"
-	cp -v /tmp/tarball.tgz $DIR
-	echo "CRC[md5]: $(md5sum /tmp/tarball.tgz | cut -d' ' -f1)  SIZE[byte]: $(stat -c%s /tmp/tarball.tgz)  FILE: 'tarball.tgz'" >"$DIR/info.txt"
+	MD5="$( md5sum "$TARBALL" | cut -d' ' -f1 )"
+	SIZE="$( stat -c%s "$TARBALL" )"
+
+	cp -v "$TARBALL" "$DIR"
+	echo "CRC[md5]: $MD5  SIZE[byte]: $SIZE  FILE: 'tarball.tgz'" >"$DIR/info.txt"
 } done
