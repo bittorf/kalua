c5d28c423a3a5fd2e994ebfac2c16886dda196fc
Bastian Bittorf <bittorf@bluebottle.com>
2016-08-17 07:56:45 +0200

    kalua: init: ffweimar: activate roaming

--- openwrt-addons/etc/hotplug.d/button/events.sh
+++ openwrt-addons/etc/hotplug.d/button/events.sh
@@ -109,7 +109,7 @@ case "${BUTTON}-${ACTION}" in
 		fi
 
 		. /tmp/loader
-		_weblogin authserver_message "button_pressed.$NODENUMBER.${LANADR:-$WANADR}.$HOSTNAME.$DIFF.msec"
+		_weblogin authserver_message "button_pressed.$NODENUMBER.${LANADR:-${WANADR:-$WIFIADR}}.$HOSTNAME.$DIFF.msec"
 
 		[ $DIFF -gt 300 ] && {
 			_log it firmware_button daemon info "button_pressed.$NODENUMBER.${LANADR:-$WANADR}.$HOSTNAME.$DIFF.msec"
