aa9162bb3ae838956ced730923fe029e85321f7c
Bastian Bittorf <bb@npl.de>
2016-12-09 21:15:53 +0100

    kalua: cgi-bin-webcam.sh: some documentation

--- openwrt-addons/www/cgi-bin-webcam.sh
+++ openwrt-addons/www/cgi-bin-webcam.sh
@@ -21,3 +21,26 @@ ls -1t *.jpg | grep -v 'lastsnap.jpg' >"$LIST"
 }
 
 rm "$LIST"
+
+pics2movie()
+{
+	local file
+	local line i=0
+
+	mkdir 'pix' 'frames'
+
+	for file in *.tar; do {
+		tar -C 'pix' -xf "$file" || echo "error in '$file'"
+
+		cd 'pix' || return 1
+		ls -1rt >../frames/files.txt
+		while read -r line; do {
+			mv "$line" "../frames/img-$( printf '%05d' "$i" ).jpg"
+			i=$(( i + 1 ))
+		} done <../frames/files.txt
+		cd - >/dev/null
+	} done
+
+	ffmpeg -r 60 -f image2 -i frames/img-%05d.jpg -vcodec libx264 -crf 15 -pix_fmt yuv420p out.mp4
+	rm -fR 'pix' 'frames'
+}
