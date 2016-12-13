5cfc23aa9c73271ae4e61b87ff59976397fa06ca
Bastian Bittorf <bb@npl.de>
2016-12-13 19:36:38 +0100

    kalua: date: sunrise() introduce new class and sunrise/sunfall

--- openwrt-addons/www/cgi-bin-webcam.sh
+++ openwrt-addons/www/cgi-bin-webcam.sh
@@ -31,33 +31,44 @@ rm "$LIST"
 
 pics2movie()
 {
-	local file
-	local line i=0
+	local file timestamp line j=0 i=0 oldest=0 newest=9999999999
 
 	mkdir 'pix' || return		# plain .jpg's from tar-files
 	mkdir 'frames' || return	# sanitized pics converted to .png
 
+	# TODO: do most in ram-disc?
+	# TODO: show progress
+	# unpack every tar and throw all files into 1 dir
 	for file in *.tar; do {
-		tar -C 'pix' -xf "$file" || echo "error in '$file'"
+		j=$(( j + 1 ))
+		tar -C 'pix' -xf "$file" || logger -s "error in '$file'"
+		rm "$file"
 
 		cd 'pix' || return 1
 		ls -1 >"../frames/files.txt"
 		while read -r line; do {
+			timestamp="$( date +%s -r "$line" )"
+			test $timestamp -gt $oldest && oldest=$timestamp
+			test $timestamp -lt $newest && newest=$timestamp
+
 			mv "$line" "../frames/img-$( date +%s -r "$line" ).jpg"
 			i=$(( i + 1 ))
 		} done <"../frames/files.txt"
 		cd - >/dev/null || return
 	} done
+	logger -s "all files: $i in $j tars - oldest=$oldest=$( date -d @$oldest ) newest=$newest=$( date -d @$newest )"
 
+	# sanitize each picture
 	cd frames || return
-	i=0
-	ls -1rt | while read -r line; do {
-		# TODO: enforce '1280x720' and RGB24?
-		convert "$line" "img-$( printf '%05d' "$i" ).png" && i=$(( i + 1 ))
-		rm "$line"
+	j=0
+	ls -1rt | while read -r file; do {
+		convert "$file" -resize 1280x720 -depth 24 "img-$( printf '%05d' "$j" ).png" && j=$(( j + 1 ))
+		logger -s "convert: $j/$i"
+		rm "$file"
 	} done
+	logger -s "all files: $i files ok: $( ls -l *.png | tail -n1 ) oldest: $oldest newest: $newest"
 
-	ffmpeg -r 60 -f image2 -i img-%05d.png -vcodec libx264 -crf 15 -pix_fmt yuv420p ../out.mp4
+	ffmpeg -r 60 -f image2 -i img-%05d.png -vcodec libx264 -crf 15 -pix_fmt yuv420p "../out.mp4"
 	cd - >/dev/null || return
 	rm -fR 'pix' 'frames'
 }
