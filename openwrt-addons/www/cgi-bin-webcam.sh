a87a43833e4ccd326ca5a76d3b33674a519fd963
Bastian Bittorf <bb@npl.de>
2016-12-16 11:05:29 +0100

    kalua: www/cgi-bin-webcam.sh: some tweaks

--- openwrt-addons/www/cgi-bin-webcam.sh
+++ openwrt-addons/www/cgi-bin-webcam.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+[ -n "$REMOTE_ADDR" ] && {
 . /tmp/loader
 
 cd /webcam || exit
@@ -16,7 +17,8 @@ ls -1t *'.jpg' | grep -v 'lastsnap.jpg' >"$LIST"
 	FILE_END="$( tail -c2 "$FILE" )"
 	test "$END_MARKER" = "$FILE_END" || BADFILE="$FILE"	# we do not remove it later, if not fully written
 
-	# e.g. download with 'wget --content-disposition http://$IP/cgi-bin-webcam.sh'
+	# e.g. download/cronjob like:
+	# wget -O recent.bin http://$IP/cgi-bin-webcam.sh && mv recent.bin webcam_$IP.$(date +%s).tar'
 	tar -c -T "$LIST" -f -
 
 	while read -r FILE; do {
@@ -28,19 +30,16 @@ ls -1t *'.jpg' | grep -v 'lastsnap.jpg' >"$LIST"
 }
 
 rm "$LIST"
+}
 
 pics2movie()
 {
-	local file timestamp line j=0 i=0 oldest=0 newest=9999999999
-	local TMPDIR="$HOME/ramdisk"
-
-	# mkdir "$HOME/ramdisk"
-	# sudo mount -t tmpfs -o size=80% none "$HOME/ramdisk"	# TODO: use it
-
+	local out codec file timestamp line j=0 i=0 oldest=0 newest=9999999999
+	# scp 10.63.2.34:bigbrother/cam-{buero-innen,buero-aussen,muelltonne}/*.tar .
 	mkdir 'pix' || return		# plain .jpg's from tar-files
 	mkdir 'frames' || return	# sanitized pics converted to .png
 
-	# unpack every tar and throw all files into 1 dir:
+	# unpack every tar and throw all files into dir 'pix' and order them
 	for file in *.tar; do {
 		j=$(( j + 1 ))
 		tar -C 'pix' -xf "$file" || logger -s "error in '$file'"
@@ -59,24 +58,22 @@ pics2movie()
 		cd - >/dev/null || return
 	} done
 	logger -s "all files: $i in $j tars - oldest=$oldest=$( date -d @$oldest ) newest=$newest=$( date -d @$newest )"
+	# Di 13. Dez 14:54:04 CET 2016
+	# di 13  dez 14 54 04 cet 2016
+	# 2016-13dez-14h54
+	set -- $( date -d @$oldest | sed 's/[^0-9a-zA-Z]/ /g' | tr '[[:upper:]]' '[[:lower:]]' )
+	oldest="$8-${2}$3-${4}h$5"
+	set -- $( date -d @$newest | sed 's/[^0-9a-zA-Z]/ /g' | tr '[[:upper:]]' '[[:lower:]]' )
+	newest="$8-${2}$3-${4}h$5"
+	out="../$( pwd | sed 's|^.*/||' )_$( date +%s )_${newest}_${oldest}.mp4"	# dirname in which we are in, e.g. cam-buero-aussen
 
-	# sanitize each picture:
+	# sanitize each picture in 'frames'
 	cd frames || return
-	j=0
+	codec='-c:v libx264 -pix_fmt yuv420p -preset ultrafast -crf 15 -profile:v baseline -level 3.0'
 	ls -1rt | while read -r file; do {
-		convert "$file" \
-			-resize 1280x720 \
-			-depth 24 -colorspace RGB \
-			-define png:compression-filter=1 \
-			-define png-compression-level=7 \
-			-define png:compression-strategy=2 \
-			"img-$( printf '%05d' "$j" ).png" && j=$(( j + 1 ))
-		logger -s "convert: $j/$i"
+		convert "$file" -resize 1280x720 -depth 24 -colorspace RGB ppm:-
 		rm "$file"
-	} done
-	logger -s "all files: $i files ok: $( ls -l *.png | tail -n1 ) oldest: $oldest newest: $newest"
+	} done | ffmpeg -r 10 -f image2pipe -vcodec ppm -i - $codec -f mp4 "$out"
 
-	ffmpeg -r 60 -f image2 -i img-%05d.png -vcodec libx264 -crf 15 -pix_fmt yuv420p "../out.mp4" || return 1
-	cd - >/dev/null || return 1
-	rm -fR 'pix' 'frames'
+	cd - >/dev/null && rm -fR 'pix' 'frames'
 }
