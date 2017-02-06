29b4198b8cc17fff17098a69cebef0905c701071
Bastian Bittorf <bb@npl.de>
2017-02-06 08:53:32 +0100

    build: fix ath25 for r49276 - 4/4

--- openwrt-build/build.sh
+++ openwrt-build/build.sh
@@ -862,7 +862,7 @@ EOF
 					FILENAME_SYSUPGRADE="openwrt-ath25-${version}-combined.squashfs.img"
 					FILENAME_FACTORY=
 				elif [ $( openwrt_revision_number_get ) -gt 49276 ]; then
-					FILENAME_SYSUPGRADE="openwrt-ath25-${version}-squashfs-sysupgrade.bin"
+					FILENAME_SYSUPGRADE="openwrt-ath25-squashfs-sysupgrade.bin"
 					FILENAME_FACTORY=
 				else
 					FILENAME_SYSUPGRADE='openwrt-atheros-combined.squashfs.img'
@@ -891,7 +891,7 @@ EOF
 					FILENAME_SYSUPGRADE="openwrt-ath25-${version}-squashfs-sysupgrade.bin"
 					FILENAME_FACTORY=
 				elif [ $( openwrt_revision_number_get ) -gt 49276 ]; then
-					FILENAME_SYSUPGRADE="openwrt-ath25-${version}-squashfs-sysupgrade.bin"
+					FILENAME_SYSUPGRADE="openwrt-ath25-squashfs-sysupgrade.bin"
 					FILENAME_FACTORY=
 				else
 					FILENAME_SYSUPGRADE='openwrt-atheros-combined.squashfs.img'
