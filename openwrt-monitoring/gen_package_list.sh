cc9c8a919c529749a784a3a54b6d4649317651fb
Bastian Bittorf <bb@npl.de>
2017-05-03 09:24:39 +0200

    monitoring: sync changes (support 'Installed-Size' in gen_package_list.sh)

--- openwrt-monitoring/gen_package_list.sh
+++ openwrt-monitoring/gen_package_list.sh
@@ -22,14 +22,14 @@ DIR=
 	cd "$DIR" || exit
 }
 
-# fixme! when in 'strip'-mode, make an directory mini/
+# FIXME! when in 'strip'-mode, make an directory mini/
 # and copy all packages in there, after fetching control-file
 # and rename to a.ipk b.ipk c.ipk
 # namespace when using:
 # a-zA-Z0-9     = 26 + 26 + 10 = 62 files
 # a-zA-Z0-9 x 2 = 3844 files
 
-OUT="Packages"
+OUT='Packages'
 [ -e "$OUT" ] && rm "$OUT"
 
 for FILE in $( ls -1 ./*.ipk ); do {
@@ -42,21 +42,29 @@ for FILE in $( ls -1 ./*.ipk ); do {
 			grep -v ^" " control | grep -v "^$" | while read -r LINE; do {
 				case "${LINE%: *}" in
 					Package|Version)
-						echo "$LINE" >>"$OUT"
+						echo "$LINE"
 					;;
 				esac
 			} done
 			
-			stat >>"$OUT" --printf "Filename: %n\n\n" "$( basename "$FILE" )"
+			stat --printf "Filename: %n\n\n" "$( basename "$FILE" )"
 		;;
 		*)
-			grep -v ^" " control | grep -v "^$" >>"$OUT"	# strip lines beginning with space and empty lines
-			stat >>"$OUT" --printf "Size: %s\nFilename: %n\n\n" "$( basename "$FILE" )"
+			# strip lines beginning with space and empty lines
+			grep -v ^' ' 'control' | grep -v "^$"
+
+			case "$FILE" in
+				*'mysettings'*)
+					echo 'Installed-Size: 0'
+				;;
+			esac
+
+			stat --printf "Size: %s\nFilename: %n\n\n" "$( basename "$FILE" )"
 		;;
 	esac
 
-	rm -f "control" "control.tar.gz"
-} done
+	rm -f 'control' 'control.tar.gz'
+} done >>"$OUT"
 
 ls -l "$OUT"
 [ -n "$MYPWD" ] && {
