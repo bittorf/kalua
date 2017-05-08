d3fa39bdfedffbbb05e4e10f66dfa35ab654b1a4
Bastian Bittorf <bb@npl.de>
2017-05-08 11:07:23 +0200

    kalua: cron: webcam: allow 'fswebcam' OR 'motion' to be installed - this should be enough

--- openwrt-addons/www/cgi-bin-webcam.sh
+++ openwrt-addons/www/cgi-bin-webcam.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+# e.g. download webcam-pictures via cronjob like:
+# wget -O recent.bin http://$IP/cgi-bin-webcam.sh && {
+#	mv recent.bin cam_$IP.$(date +%s).tar'
+# }
+
+# we ignore this port when sourcing script for just using pics2movie()
 [ -n "$REMOTE_ADDR" ] && {
 . /tmp/loader
 
@@ -17,14 +24,13 @@ sed -i '/.*lastsnap.jpg/d' "$LIST"
 	FILE_END="$( tail -c2 "$FILE" )"
 	test "$END_MARKER" = "$FILE_END" || BADFILE="$FILE"	# we do not remove it later, if not fully written
 
-	# e.g. download/cronjob like:
-	# wget -O recent.bin http://$IP/cgi-bin-webcam.sh && mv recent.bin cam_$IP.$(date +%s).tar'
 	tar -c -T "$LIST" -f -
 
 	while read -r FILE; do {
-		# test "$END_MARKER" = "$FILE_END"
 		test -e "$FILE" -a "$FILE" != './webcam.jpg' && {
-			test "$FILE" = "$BADFILE" || rm "$FILE"
+			test "$FILE" = "$BADFILE" || {
+				case "$QUERY_STRING" in *'DEBUG=1'*);;*) rm "$FILE" ;; esac
+			}
 		}
 	} done <"$LIST"
 }
@@ -36,6 +42,7 @@ pics2movie()
 {
 	local resolution='1280x720'
 	local out codec file timestamp line j=0 i=0 oldest=0 newest=9999999999
+
 	# scp 10.63.2.34:bigbrother/cam-{buero-innen,buero-aussen,muelltonne}/*.tar .
 	mkdir 'pix' || return		# plain .jpg's from tar-files
 	mkdir 'frames' || return	# sanitized pics converted to .png
