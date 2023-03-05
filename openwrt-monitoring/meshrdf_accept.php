529bc4f3c628ad9802b46da3737dd845bd2eb215
Bastian Bittorf <bb@npl.de>
2023-03-05 16:11:36 +0100

    monitoring: use a workaround for php8: meshrdf_accept.php - see https://stackoverflow.com/questions/70549291/best-way-to-handle-undefined-array-key-in-php-8-0

--- openwrt-monitoring/meshrdf_accept.php
+++ openwrt-monitoring/meshrdf_accept.php
@@ -12,19 +12,24 @@ if(isset($_GET["refresh"])) {		// refresh='true' -> monitoring_heartbeat
 		// ?refresh=true&mac=14cc20ed2513&up=10&h2=98926592&load=14&version=396349&r4=0&r5=1
 
 		$v0 = $_SERVER["REMOTE_ADDR"];
-		$v1 = strval($_GET["up"]);		// uptime
-		$v2 = strval($_GET["h2"]);		// ram_free
-		$v3 = strval($_GET["load"]);		// load
-		$v4 = strval($_GET["version"]);		// kalua-version
-		$r4 = strval($_GET["r4"]);		// wifi-clients
-		$r5 = strval($_GET["r5"]);		// wired-clients
-		$n1 = strval($_GET["neigh"]);		// neighs
+		$v1 = strval($_GET["up"] ?? '');		// uptime
+		$v2 = strval($_GET["h2"] ?? '');		// ram_free
+		$v3 = strval($_GET["load"] ?? '');		// load
+		$v4 = strval($_GET["version"] ?? '');		// kalua-version
+		$r4 = strval($_GET["r4"] ?? '');		// wifi-clients
+		$r5 = strval($_GET["r5"] ?? '');		// wired-clients
+		$n1 = strval($_GET["neigh"] ?? '');		// neighs
+		$rrr = strval($_GET["rrr"] ?? '');		// experimental for map-drawing (e.g. default route)
 
 		// FIXME! keep latest neigh-info somehow
 		if (!empty($n1)) {
-			file_put_contents("recent/".$mac.".changes", "UP=".$v1.";LOAD=".$v3.";h2=".$v2.";VERSION=".$v4.";r4=".$r4.";r5=".$r5.";PUBIP_REAL=".$v0.";neigh=".$n1.";");
+			file_put_contents("recent/".$mac.".changes", "UP=".$v1.";LOAD=".$v3.";h2=".$v2.";VERSION=".$v4.";r4=".$r4.";r5=".$r5.";PUBIP_REAL=".$v0.";rrr=".$rrr.";neigh=".$n1.";");
 		} else {
-			file_put_contents("recent/".$mac.".changes", "UP=".$v1.";LOAD=".$v3.";h2=".$v2.";VERSION=".$v4.";r4=".$r4.";r5=".$r5.";PUBIP_REAL=".$v0.";");
+			if (!empty($rrr)) {
+				file_put_contents("recent/".$mac.".rrr", "UP=".$v1.";LOAD=".$v3.";h2=".$v2.";VERSION=".$v4.";r4=".$r4.";r5=".$r5.";PUBIP_REAL=".$v0.";rrr='".$rrr."';");
+			} else {
+				file_put_contents("recent/".$mac.".changes", "UP=".$v1.";LOAD=".$v3.";h2=".$v2.";VERSION=".$v4.";r4=".$r4.";r5=".$r5.";PUBIP_REAL=".$v0.";rrr=".$rrr.";");
+			};
 		};
 
 		$pwd = getcwd();
@@ -39,6 +44,7 @@ if(isset($_GET["refresh"])) {		// refresh='true' -> monitoring_heartbeat
 			print("REFRESHED");
 		};
 	} else {
+		touch("recent/$mac");		// security problem?
 		print("UNKNOWN");
 	}
 
@@ -86,63 +92,63 @@ if(!isset($_GET["log"]) && !isset($_GET["local"]) ){
 	exit;
 };
 
-$e0 = strval($_GET["e0"]);			// how many error's during send for this monitoring-message
-$e1 = strval($_GET["e1"]);			// how many error's during send for all monitoring-messages since last reboot
+$e0 = strval($_GET["e0"] ?? '');			// how many error's during send for this monitoring-message
+$e1 = strval($_GET["e1"] ?? '');			// how many error's during send for all monitoring-messages since last reboot
 
-$D0 = strval($_GET["D0"]);			// dhcpscript: bool (off/on)
+$D0 = strval($_GET["D0"] ?? '');			// dhcpscript: bool (off/on)
 
-$k0 = strval($_GET["k0"]);			// seconds from klog when RT throttling was activated
-$k1 = strval($_GET["k1"]);			// linecount dmesg.boot
-$k2 = strval($_GET["k2"]);			// linecount dmesg.log
-$k3 = strval($_GET["k3"]);			// linecount coredumps
+$k0 = strval($_GET["k0"] ?? '');			// seconds from klog when RT throttling was activated
+$k1 = strval($_GET["k1"] ?? '');			// linecount dmesg.boot
+$k2 = strval($_GET["k2"] ?? '');			// linecount dmesg.log
+$k3 = strval($_GET["k3"] ?? '');			// linecount coredumps
 
-$u0 = strval($_GET["u0"]);			// usb plugged in? 1=yes
+$u0 = strval($_GET["u0"] ?? '');			// usb plugged in? 1=yes
 
-$w0 = strval($_GET["w0"]);			// name wifidev
-$w1 = strval($_GET["w1"]);			// restarts
-$w2 = strval($_GET["w2"]);			// uptime
-$w3 = strval($_GET["w3"]);			// last reason for restart
+$w0 = strval($_GET["w0"] ?? '');			// name wifidev
+$w1 = strval($_GET["w1"] ?? '');			// restarts
+$w2 = strval($_GET["w2"] ?? '');			// uptime
+$w3 = strval($_GET["w3"] ?? '');			// last reason for restart
 
-$t0 = strval($_GET["t0"]);			// olsr_wifi_in
-$t1 = strval($_GET["t1"]);			// olsr_wifi_out
-$t2 = strval($_GET["t2"]);			// olsr_speed
-$t3 = strval($_GET["t3"]);			// olsr_metric
+$t0 = strval($_GET["t0"] ?? '');			// olsr_wifi_in
+$t1 = strval($_GET["t1"] ?? '');			// olsr_wifi_out
+$t2 = strval($_GET["t2"] ?? '');			// olsr_speed
+$t3 = strval($_GET["t3"] ?? '');			// olsr_metric
 
-$d0 = strval($_GET["d0"]);			// database last line
-$d1 = strval($_GET["d1"]);			// enforced database auth server
+$d0 = strval($_GET["d0"] ?? '');			// database last line
+$d1 = strval($_GET["d1"] ?? '');			// enforced database auth server
 
-$i0 = strval($_GET["i0"]);			// inet: proto
-$i1 = strval($_GET["i1"]);			// inet: interface
-$i2 = strval($_GET["i2"]);			// inet: ip/pre
-$i3 = strval($_GET["i3"]);			// inet: downstream [kbyte/s]
-$i4 = strval($_GET["i4"]);			// inet: upstream [kbyte/s]
-$i5 = strval($_GET["i5"]);			// inet: gateway-ip
-$i6 = strval($_GET["i6"]);			// inet: speed to authserver
+$i0 = strval($_GET["i0"] ?? '');			// inet: proto
+$i1 = strval($_GET["i1"] ?? '');			// inet: interface
+$i2 = strval($_GET["i2"] ?? '');			// inet: ip/pre
+$i3 = strval($_GET["i3"] ?? '');			// inet: downstream [kbyte/s]
+$i4 = strval($_GET["i4"] ?? '');			// inet: upstream [kbyte/s]
+$i5 = strval($_GET["i5"] ?? '');			// inet: gateway-ip
+$i6 = strval($_GET["i6"] ?? '');			// inet: speed to authserver
 
-$r0 = strval($_GET["r0"]);			// rssi wlan0
-$r1 = strval($_GET["r1"]);			// rssi wlan1
-$r2 = strval($_GET["r2"]);			// rssi wlan2
-$r3 = strval($_GET["r3"]);			// rssi wlan3
-$r4 = strval($_GET["r4"]);			// wifi_clients_sum
-$r5 = strval($_GET["r5"]);			// wired_clients_sum
-$r9 = strval($_GET["r9"]);			// reboot_reason
+$r0 = strval($_GET["r0"] ?? '');			// rssi wlan0
+$r1 = strval($_GET["r1"] ?? '');			// rssi wlan1
+$r2 = strval($_GET["r2"] ?? '');			// rssi wlan2
+$r3 = strval($_GET["r3"] ?? '');			// rssi wlan3
+$r4 = strval($_GET["r4"] ?? '');			// wifi_clients_sum
+$r5 = strval($_GET["r5"] ?? '');			// wired_clients_sum
+$r9 = strval($_GET["r9"] ?? '');			// reboot_reason
 
-$h0 = strval($_GET["h0"]);			// simple_meshnode = 1
-$h1 = strval($_GET["h1"]);			// ram_size
-$h2 = strval($_GET["h2"]);			// ram_free
-$h3 = strval($_GET["h3"]);			// ram_free flushed
-$h4 = strval($_GET["h4"]);			// zram reads
-$h5 = strval($_GET["h5"]);			// zram writes
-$h6 = strval($_GET["h6"]);			// zram memory_needed
-$h7 = strval($_GET["h7"]);			// zram_compressed_size
+$h0 = strval($_GET["h0"] ?? '');			// simple_meshnode = 1
+$h1 = strval($_GET["h1"] ?? '');			// ram_size
+$h2 = strval($_GET["h2"] ?? '');			// ram_free
+$h3 = strval($_GET["h3"] ?? '');			// ram_free flushed
+$h4 = strval($_GET["h4"] ?? '');			// zram reads
+$h5 = strval($_GET["h5"] ?? '');			// zram writes
+$h6 = strval($_GET["h6"] ?? '');			// zram memory_needed
+$h7 = strval($_GET["h7"] ?? '');			// zram_compressed_size
 
-$s1 = strval($_GET["s1"]);			// switch
-$s2 = strval($_GET["s2"]);			// lan-dhcp-ignore
+$s1 = strval($_GET["s1"] ?? '');			// switch
+$s2 = strval($_GET["s2"] ?? '');			// lan-dhcp-ignore
 
-$v1 = strval($_GET["v1"]);			// kernel
-$v2 = strval($_GET["v2"]);			// git
+$v1 = strval($_GET["v1"] ?? '');			// kernel
+$v2 = strval($_GET["v2"] ?? '');			// git
 
-$n0 = strval($_GET["n0"]);			// wired neighs sorted
+$n0 = strval($_GET["n0"] ?? '');			// wired neighs sorted
 
 $wifiscan = strval($_GET["wifiscan"]);		//
 $sens = strval($_GET["sens"]);			// sensitivity
@@ -195,7 +201,7 @@ $pfilter = strval($_GET["pfilter"]);
 $olsrrestartcount = strval($_GET["olsrrestartcount"]);
 $olsrrestarttime = strval($_GET["olsrrestarttime"]);
 
-$secret = strval($_GET["secret"]);		// hex
+$secret = strval($_GET["secret"] ?? '');	// hex
 if(!ctype_xdigit($secret)) {
 	$secret = "";
 };
